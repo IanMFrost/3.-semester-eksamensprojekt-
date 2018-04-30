d8fbdc38a5fd3d917f010941b81d39401d9000a0
wandriey <jona434g@edu.easj.dk>
2018-04-30 10:33:39 +0200

    Udregning klasse og tilsvarende test
    Skal bruges til at test vores udviklingsmiljø

---
+++ ConsoleApp1/ConsoleApp1/Udregning.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Udregning
+    {
+        public double Division(double x, double y)
+        {
+            return x / y;
+        }
+        public double Gange(double x, double y)
+        {
+            return x * y;
+        }
+        public double Plus(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1Tests/UdregningTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class UdregningTests
+    {
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            Udregning udregning = new Udregning();
+
+            double actualvalue = udregning.Division(10, 2);
+            double expectedvalue = 5;
+
+
+            Assert.AreEqual(expectedvalue, actualvalue);
+        }
+
+        [TestMethod()]
+        public void GangeTest()
+        {
+            Udregning udregning = new Udregning();
+
+            double actualvalue = udregning.Gange(10, 2);
+            double expectedvalue = 20;
+
+
+            Assert.AreEqual(expectedvalue, actualvalue);
+        }
+
+        [TestMethod()]
+        public void PlusTest()
+        {
+            Udregning udregning = new Udregning();
+
+            double actualvalue = udregning.Plus(4, 2);
+            double expectedvalue = 6;
+
+            Assert.AreEqual(expectedvalue, actualvalue);
+        }
+    }
+}
