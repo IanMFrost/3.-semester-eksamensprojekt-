e0efcd9b65b5553a0491c924398544a1d51834c8
IanMFrost <ianmfrost0@gmail.com>
2018-04-30 11:18:02 +0200

    ændret expectedValue

--- ConsoleApp1/ConsoleApp1Tests/UdregningTests.cs
+++ ConsoleApp1/ConsoleApp1Tests/UdregningTests.cs
@@ -40,8 +40,8 @@ namespace ConsoleApp1.Tests
         {
             Udregning udregning = new Udregning();
 
-            double actualvalue = udregning.Plus(2, 2);
-            double expectedvalue = 4;
+            double actualvalue = udregning.Plus(4, 2);
+            double expectedvalue = 6;
 
             Assert.AreEqual(expectedvalue, actualvalue);
         }
